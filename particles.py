dfa8110465f18a52273d41665d60a24e753a40a9
nhoogendam <nhoogendam@gmail.com>
2018-02-26 20:17:06 -0600

    Merge branch 'master' of https://github.com/zipppzy/Powder
    # Conflicts:
    #	particles.py

--- particles.py
+++ particles.py
@@ -9,11 +9,5 @@ class Particle:
         self.x += x
         self.y += y
 
-<<<<<<< Updated upstream
-    def xSpeed(self, x):
-        self.x += x
-=======
     def xAccel(self, x):
         self.xSpeed += x
-
->>>>>>> Stashed changes
