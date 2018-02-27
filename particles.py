82d17d70f10cf1b1c9fcd394deff29a2b3659fec
Zubin Hathiram <zipppzy939@gmail.com>
2018-02-26 18:17:15 -0600

    first commit

---
+++ particles.py
@@ -0,0 +1,11 @@
+class Particle:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.xSpeed = 0
+        self.ySpeed = 0
+
+    def move(self, x, y):
+        self.x += x
+        self.y += y
+
