d748d64ab4d75ed4a5e15a3c37d4841cd5b60d67
nhoogendam <nhoogendam@gmail.com>
2018-03-05 16:00:02 -0600

    Merge branch 'master' of https://github.com/zipppzy/Powder
    # Conflicts:
    #	particles.py

--- particles.py
+++ particles.py
@@ -5,10 +5,15 @@ class Particle:
         self.xSpeed = 0
         self.ySpeed = 0
 
+    #Vector
     def move(self, x, y):
         self.x += x
         self.y += y
 
+    #Accelerating particle
     def accel(self, x, y):
         self.xSpeed += x
         self.ySpeed += y
+
+#class that inherits method from Particle class
+#class Powder(Particle):
